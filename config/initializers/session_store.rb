3ffa1d21c6648c68c5324648d9a30ebe86562a74
denny2trasy <denny2trasy@gmail.com>
2014-03-17 11:42:08 +0800

    new session token

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,5 +5,5 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# ActiveRecord::SessionStore::Session.acts_as_remote :el_user, :readonly => false
-# ElCourse::Application.config.session_store :active_record_store
+ActiveRecord::SessionStore::Session.acts_as_remote :el_user, :readonly => false
+ElCourse::Application.config.session_store :active_record_store
